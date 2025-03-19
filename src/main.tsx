593ea0487a60a3d559303948d64e8be45a9e2a03
Nasimikh91 <73756566+Nasimikh91@users.noreply.github.com>
2025-03-19 03:59:34 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+
 // Mount the app to the DOM
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error('Failed to find the root element');
